2051be21bd1ead2d3920a50a5580e7288c15992f
André Miranda <aegis.andre@gmail.com>
2024-05-31 11:21:16 -0300

    chore: add eslint and prettier and remove boilerplate

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+
+const config = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  trailingComma: "es5",
+  arrowParens: "always",
+  singleQuote: false,
+  jsxSingleQuote: false,
+};
+
+module.exports = config;
